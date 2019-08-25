0832d4d5ae18e8f36308a1768fd46d4ae713d36b
Angel <as.sosaramos@gmail.com>
2019-08-25 12:44:21 -0500

    routing - generos, peliculas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { GeneroComponent } from './_pages/genero/genero.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeliculaComponent } from './_pages/pelicula/pelicula.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'genero', component: GeneroComponent},
+  {path: 'pelicula', component: PeliculaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GeneroComponent } from './_pages/genero/genero.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PeliculaComponent } from './_pages/pelicula/pelicula.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GeneroComponent
+    GeneroComponent,
+    PeliculaComponent
   ],
   imports: [
     BrowserModule,
